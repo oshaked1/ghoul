3eb71eba6bc717acab7231ee132dc81092294d62
Ofek Shaked <oshaked1@gmail.com>
2022-07-29 21:54:09 +0000

    Added test for change_fd service (#8)

---
+++ tests/test_change_fd.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath('.'))
+import ghoulctl
+
+
+def test_change_fd():
+    # make sure conditions are right
+    assert ghoulctl.ping()
+
+    # perform test
+    ghoulctl.change_fd(ghoulctl.service_fd + 1)
+    
+    # ghoulctl keeps track of fd so no need to manually set it for next requests
+    assert ghoulctl.ping()
+
+    # revert change
+    ghoulctl.change_fd(ghoulctl.service_fd - 1)
+    assert ghoulctl.ping()
