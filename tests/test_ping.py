c00d5b87a5293c16e896de4f415a49105faac515
Ofek Shaked <oshaked1@gmail.com>
2022-07-29 21:58:26 +0000

    Added tests for ping and change_ping_arg services (#8)

---
+++ tests/test_ping.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath('.'))
+import ghoulctl
+
+
+def test_ping():
+    assert ghoulctl.ping()
+
+
+def test_ping_wrong_arg():
+    # make sure conditions are right
+    assert ghoulctl.ping()
+
+    # perform test
+    assert not ghoulctl.ping(ghoulctl.ping_arg + 1)
+
+
+def test_change_ping_arg():
+    # make sure conditions are right
+    assert ghoulctl.ping()
+
+    # perform test
+    ghoulctl.change_ping_arg(ghoulctl.ping_arg + 1)
+
+    # ghoulctl keeps track of ping arg so no need to manually set it for next requests
+    assert ghoulctl.ping()
+
+    # revert change
+    ghoulctl.change_ping_arg(ghoulctl.ping_arg - 1)
+    assert ghoulctl.ping()
